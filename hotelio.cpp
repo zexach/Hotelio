f53e7d6bdcc301f7445cbf80a06baf1438cca40b
ajdinbukvic <83658573+ajdinbukvic@users.noreply.github.com>
2021-05-05 11:57:06 +0200

    Update hotelio.cpp

--- hotelio.cpp
+++ hotelio.cpp
@@ -1,6 +1,51 @@
-#include<iostream>
+/*|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+||															                             ||
+||				Naziv projekta: Hotelio						                 ||
+||				Zavrsni projekat iz predmeta:				               ||
+||				Napredne tehnike programiranja				             ||
+||				Mentor: v. as. mr. Edin Tabak				               ||
+||						Autori:								                         ||
+||								Muamer Alickovic			                     ||
+||								Ajdin Bukvic				                       ||
+||								Emir Zambakovic				                     ||
+||															                             ||
+|| 						© 2021 								                         ||
+||															                             ||
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+
+//-------------------- >>> BIBLIOTEKE <<< ---------------------
+
+#include <iostream>
+#include <conio.h>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <windows.h>
+#include <cstdlib>
+#include <ctime>
+#include <cctype>
+#include <fstream>
+#include <algorithm>
+#include <cmath>
 using namespace std;
 
-int main(){
-  return 0;
+//-------------------- >>> KONSTANTE <<< ---------------------
+
+//-------------------- >>> GLOBALNE VARIJABLE <<< ---------------------
+char *linija = "\n=========================================================\n";
+char *greska = "[GRESKA]: Pogresan unos, pokusajte ponovo!\n";
+//-------------------- >>> ENUMERACIJE <<< ---------------------
+
+//-------------------- >>> STRUKTURE <<< ---------------------
+
+//-------------------- >>> SPISAK FUNKCIJA <<< ---------------------
+
+//-------------------- >>> MAIN FUNKCIJA <<< ---------------------
+
+int main()
+{
+	
+	return 0;
 }
+
+//-------------------- >>> FUNKCIJE <<< ---------------------
