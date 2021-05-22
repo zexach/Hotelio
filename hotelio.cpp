71b35e6c4b23450633507eb1c32b202da4ccd178
ajdinbukvic <83658573+ajdinbukvic@users.noreply.github.com>
2021-05-23 01:59:57 +0200

    Update hotelio.cpp

--- hotelio.cpp
+++ hotelio.cpp
@@ -39,7 +39,7 @@ enum velicinaSobe {
     jednokrevetna=1,
     dvokrevetna,
     trokrevetna,
-    predsjednickiApartman
+	predsjednickiApartman
 };
 
 //enumeracija za poziciju radnika
@@ -51,39 +51,46 @@ enum pozicijaRadnika {
     trener
 };
 
+enum stanje_rezervacije {
+	cekanje,
+	prihvaceno,
+	odbijeno
+};
+
 //-------------------- >>> STRUKTURE <<< ---------------------
 //struktura soba
-struct Soba {
-	velicinaSobe velicina;
-	float cijena;
-	int brojSobe;
-	bool slobodna;
+struct Soba{
+    velicinaSobe velicina;
+    float cijena;
+    int brojSobe;
+    bool slobodna;
 };
 
 //struktura datum
 struct Datum {
-	int dan,mjesec,godina;
+    int dan,mjesec,godina;
 };
 //struktura rezervacija
-struct Rezervacija {
-	string ime;
-	string prezime;
+struct Rezervacija{
+	int kod;
+	int broj_sobe;
+	string username;
 	Datum datumPrijave;
 	Datum datumOdjave;
-
+	stanje_rezervacije stanje;
 };
 
 //struktura radnik
-struct Radnik {
-	int id;
-	string ime;
-	string prezime;
-	pozicijaRadnika pozicija;
-	double plata;
+struct Radnik{
+    int id;
+    string ime;
+    string prezime;
+    pozicijaRadnika pozicija;
+    double plata;
 };
 
 //struktura korisnik
-struct Korisnik {
+struct Korisnik{
 	string ime;
 	string prezime;
 	string spol;
@@ -92,7 +99,7 @@ struct Korisnik {
 };
 
 //struktura hotel
-struct Hotel {
+struct Hotel{
 	string nazivHotela;
 	double stanjeKase;
 	int brojSoba;
@@ -106,9 +113,16 @@ void poruka(int);
 int glavniMenu();
 void registracija();
 int prebroj_clanove(string);
+string skrivena_sifra(string,char);
 void prijava(string&,string&,bool&);
 void adminPrijava(bool&);
 int admin_menu();
+void prikazi_rezervacije();
+void prihvati_odbij_rezervaciju();
+void azuriraj_rezervacije(Rezervacija*,int,int,int);
+void pretrazi_sobe();
+void uredi_cjenovnik();
+void azuriraj_sobe(Soba*,int,int,float);
 //-------------------- >>> MAIN FUNKCIJA <<< ---------------------
 
 int main() {
@@ -141,22 +155,22 @@ int main() {
 						switch (odabir) {
 							case 1: {
 								system("cls");
-								//
+								prikazi_rezervacije();
 								break;
 							}
 							case 2: {
 								system("cls");
-								//
+								prihvati_odbij_rezervaciju();
 								break;
 							}
 							case 3: {
 								system("cls");
-								//
+								pretrazi_sobe();
 								break;
 							}
 							case 4: {
 								system("cls");
-								//
+								uredi_cjenovnik();
 								break;
 							}
 							case 5: {
@@ -184,10 +198,9 @@ int main() {
 								//
 								break;
 							}
-							case 10: {
-								exit(EXIT_FAILURE);
-							}
 						}
+						poruka(2);
+						getch();
 					}
 				}
 				break;
@@ -264,15 +277,7 @@ int glavniMenu() {
 //funkcija za registraciju novog korisnika
 void registracija() {
 	int brojac,pomocnaVarijabla=0; //u ovu varijablu spremamo broj clanova u datoteci
-	ifstream ulaz("listaKorisnika.txt");
-	if(ulaz.is_open()){
-		brojac=prebroj_clanove("listaKorisnika.txt");
-		ulaz.close();
-	}
-	else{
-		poruka(5);
-		return;
-	}
+	brojac=prebroj_clanove("listaKorisnika.txt");
 	Korisnik* korisnici=new Korisnik[brojac];
 	//sada cemo kopirati sve elemente u nas dinamicki niz;
 	ifstream ulaz1("listaKorisnika.txt");
@@ -432,19 +437,13 @@ int prebroj_clanove(string naziv_datoteke) {
 	}
 	return brojac;
 }
+
 //funkcija za prijavu u sistem (prijavu korisnika)
 void prijava(string& korisnickoIme, string& sifra, bool& ispravnost) {
 	ispravnost=false;
 	bool rezultat = false;
 	int broj=0, brojElemenata, brojPokusaja=0,brojac=0;
-	ifstream ulaz("listaKorisnika.txt");
-	if(ulaz.is_open()){
-		brojElemenata = prebroj_clanove("listaKorisnika.txt");
-	}
-	else {
-		poruka(5);
-		return;
-	}
+	brojElemenata = prebroj_clanove("listaKorisnika.txt");
 	Korisnik* korisnici=new Korisnik[brojElemenata];//kreiramo niz od tacno onoliko elemenata oliko ima u datoteci
 	ifstream noviUlaz("listaKorisnika.txt"); //otvaramo datoteku drugi put kako bi kopirali elemenete u niz
 	if (noviUlaz.is_open()) {
@@ -499,8 +498,10 @@ void prijava(string& korisnickoIme, string& sifra, bool& ispravnost) {
 			Sleep(900);
 			return;
 		}
+		char znak;
 		cout<<"Unesite sifru: ";
-		cin>>sifra;
+		sifra = skrivena_sifra(sifra, znak);
+		cout<<endl;
 		for (int i = 0; i < brojElemenata; i++) {
 			if(sifra == korisnici[i].password) {
 				rezultat = true;
@@ -559,8 +560,10 @@ void adminPrijava(bool& ispravnost) {
 			Sleep(900);
 			return;
 		}
+		char znak;
 		cout<<"Unesite sifru: ";
-		cin>>sifra;
+		sifra = skrivena_sifra(sifra, znak);
+		cout<<endl;
 		if(sifra == adminPassword) {
 			rezultat = true;
 			brojac++;
@@ -573,6 +576,21 @@ void adminPrijava(bool& ispravnost) {
 		ispravnost=true;
 	}
 }
+//funkcija koja sluzi za unos sifre bez prikazivanja karaktera
+string skrivena_sifra(string sifra, char znak) {
+	sifra.clear();
+	while (znak = getch()) {
+		if (znak != 13) {
+			cout << "*";
+		}
+		sifra += znak;
+		if (znak == 13) {
+			break;
+		}
+	}
+	sifra.erase(sifra.length() - 1);
+	return sifra;
+}
 //funkcija za admin menu
 int admin_menu() {
 	system("cls");
@@ -581,22 +599,280 @@ int admin_menu() {
 	cout<<"\t\t\t\tADMIN MENU"<<endl;
 	cout << "\t\t1. Prikazi rezervacije\n";
 	cout << "\t\t2. Prihvati / odbaci rezervaciju\n";
-	cout << "\t\t3. Provjeri slobodne sobe\n";
-	cout << "\t\t4. Prikazi zauzete sobe\n";
-	cout << "\t\t5. Uredi cjenovnik\n";
-	cout << "\t\t6. Izdaj racun\n";
-	cout << "\t\t7. Pregled svih racuna\n";
-	cout << "\t\t8. Stanje hotela\n";
+	cout << "\t\t3. Pretrazi sobe po kriterijima\n";
+	cout << "\t\t4. Uredi cjenovnik\n";
+	cout << "\t\t5. Izdaj racun\n";
+	cout << "\t\t6. Pregled svih racuna\n";
+	cout << "\t\t7. Stanje hotela\n";
+	cout << "\t\t8. Dodaj radnika\n";
 	cout << "\t\t9. Informacije o radnicima\n";
-	cout << "\t\t10. Izlaz\n";
+	cout << "\t\t0. Odjava\n";
 	poruka(1);
 	int opcija;
 	cout<<"\t\tIzaberite opciju: ";
 	cin >> opcija;
 	if (opcija!=0) return opcija;
 	else {
+		cout<<"\nUspjesno ste se odjavili iz admin menua!\n";
 		cin.clear();
 		cin.ignore(100, '\n');
 		return 0;
+	}
+}
+//funkcija koja ispisuje sve rezervacije koje se nalaze u datoteci rezervacije.txt
+void prikazi_rezervacije(){
+	Rezervacija *r = new Rezervacija;
+	int broj;
+	string temp;
+	int br_rezervacija = prebroj_clanove("rezervacije.txt");
+	if(br_rezervacija == 0){
+		cout<<"Trenutno nema aktivnih rezervacija!"<<endl;
+		return;
+	}
+	cout<<"-------------------------------------------------------------------------------------\n";
+	cout<<setw(5)<<"Kod"<<setw(14)<<"Broj sobe"<<setw(16)<<"Username"<<setw(14)<<"Prijava"<<setw(14)<<"Odjava"<<setw(16)<<"Stanje"<<endl;
+	cout<<"-------------------------------------------------------------------------------------\n";
+	ifstream ulaz("rezervacije.txt");
+	if(ulaz.is_open()){
+		while(ulaz >> r->kod >> r->broj_sobe >> r->username >> r->datumPrijave.dan >> r->datumPrijave.mjesec >> r->datumPrijave.godina
+			>> r->datumOdjave.dan >> r->datumOdjave.mjesec >> r->datumOdjave.godina >> broj){
+			if(broj == 0) temp = "Cekanje";
+			else temp = "Prihvaceno";
+			cout<<setw(5)<<r->kod<<setw(12)<<r->broj_sobe<<setw(16)<<r->username<<setw(10)<<right<<r->datumPrijave.dan<<"."<<r->datumPrijave.mjesec<<"."<<r->datumPrijave.godina
+			<<setw(10)<<r->datumOdjave.dan<<"."<<r->datumOdjave.mjesec<<"."<<r->datumOdjave.godina<<setw(14)<<temp<<endl;
+		}
+		ulaz.close();
+		cout<<"-------------------------------------------------------------------------------------\n";
+		delete r;	
+	}
+	else{
+		poruka(5);
+		return;
+	}
+}
+//funkcija koja na osnovu unesenog koda rezervacije mijenja stanje rezervacije
+void prihvati_odbij_rezervaciju(){
+	Rezervacija *r = new Rezervacija;
+	int unos;
+	int izbor;
+	int stanje;
+	int br = 0;
+	int pozicija;
+	bool postoji = false;
+	int br_rezervacija = prebroj_clanove("rezervacije.txt");
+	if(br_rezervacija == 0){
+		cout<<"Trenutno nema aktivnih rezervacija!"<<endl;
+		return;
+	}
+	Rezervacija *niz = new Rezervacija[br_rezervacija];
+	cout<<"Unesite kod rezervacije koju zelite prihvatiti / odbiti: ";
+	cin>>unos;
+	do{
+		cout<<"1 - PRIHVATANJE | 2 - ODBIJANJE : ";
+		cin>>izbor;
+	}
+	while(izbor < 1 || izbor > 2);	
+	ifstream ulaz("rezervacije.txt");
+	if(ulaz.is_open()){
+		while(ulaz >> r->kod >> r->broj_sobe >> r->username >> r->datumPrijave.dan >> r->datumPrijave.mjesec >> r->datumPrijave.godina
+			>> r->datumOdjave.dan >> r->datumOdjave.mjesec >> r->datumOdjave.godina >> stanje){
+			if(stanje == 0) r->stanje = cekanje;
+			else r->stanje = prihvaceno;
+			niz[br] = *r;
+			br++;
+			if(unos == r->kod){
+				postoji = true;
+				if(izbor == 1 && r->stanje == cekanje){
+					cout<<"Uspjesno ste prihvatili rezervaciju!"<<endl;
+				}
+				else if(izbor == 2 && r->stanje == cekanje){
+					cout<<"Uspjesno ste odbili rezervaciju!"<<endl;
+				}
+				else if((izbor == 1 || izbor == 2) && r->stanje == prihvaceno){
+					cout<<"Ova rezervacija je vec prihvacena!"<<endl;
+					return;
+				}
+				pozicija = br-1;
+			}
+		}
+		ulaz.close();
+		if(!postoji){
+			cout<<"Ne postoji uneseni kod rezervacije!"<<endl;
+			return;
+		}
+		else{
+		azuriraj_rezervacije(niz, br_rezervacija, pozicija, izbor);
+		}
+		delete r;
+		delete[] niz;	
+	}
+	else{
+		poruka(5);
+		return;
+	}
+}
+//funkcija koja mijenja sve promjene rezervacija u datoteci
+void azuriraj_rezervacije(Rezervacija *niz, int br_rezervacija, int pozicija, int izbor){
+	string linija;
+	ofstream izlaz("rezervacije.txt");
+	if(izlaz.is_open()){
+		for(int i = 0; i<br_rezervacija; i++){
+			if(i == pozicija && izbor == 1){
+				izlaz<<niz[i].kod<<" "<<niz[i].broj_sobe<<" "<<niz[i].username<<" "<<niz[i].datumPrijave.dan<<" "<<niz[i].datumPrijave.mjesec<<" "<<niz[i].datumPrijave.godina<<" "
+					<<niz[i].datumOdjave.dan<<" "<<niz[i].datumOdjave.mjesec<<" "<<niz[i].datumOdjave.godina<<" "<<izbor<<endl;
+			}
+			else if(i == pozicija && izbor == 2){
+				izlaz << "odbijeno" << endl;
+			}
+			else{
+				izlaz<<niz[i].kod<<" "<<niz[i].broj_sobe<<" "<<niz[i].username<<" "<<niz[i].datumPrijave.dan<<" "<<niz[i].datumPrijave.mjesec<<" "<<niz[i].datumPrijave.godina<<" "
+					<<niz[i].datumOdjave.dan<<" "<<niz[i].datumOdjave.mjesec<<" "<<niz[i].datumOdjave.godina<<" "<<niz[i].stanje<<endl;
+			}
+		}
+	izlaz.close();	
+	}
+	else{
+		poruka(5);
+		return;
+	}
+	ifstream ulaz("rezervacije.txt");
+	if(ulaz.is_open()){
+		ofstream novi_izlaz("azurirano.txt");
+		if(novi_izlaz.is_open()){
+			while(getline(ulaz,linija)){
+				if(linija != "odbijeno"){
+					novi_izlaz << linija << endl;
+				}
+			}
+		novi_izlaz.close();	
+		}
+		else{
+			poruka(5);
+			return;
+		}
+	ulaz.close();
+	remove("rezervacije.txt");
+	rename("azurirano.txt","rezervacije.txt");
+	}
+	else{
+		poruka(5);
+		return;
+	}
+}
+//funkcija koja na osnovu unosa ispisuje sobe prema tom kriteriju
+void pretrazi_sobe(){
+	string pojam;
+	string krevet;
+	string stanje;
+	int velicina_broj;
+	int brojac = 0;
+	cout<<"Pretraga soba prema odredjenom kriteriju"<<endl;
+	cout<<"Dostupni pojmovi: jednokrevetna | dvokrevetna | trokrevetna | apartman | zauzeta | slobodna"<<endl;
+	do{
+		pojam.clear();
+		cout<<"Unesite pojam: ";
+		cin>>pojam;
+		if(pojam != "jednokrevetna" && pojam != "dvokrevetna" && pojam != "trokrevetna" && pojam != "apartman" && pojam != "zauzeta" && pojam != "slobodna")
+		poruka(3);
+	}
+	while(pojam != "jednokrevetna" && pojam != "dvokrevetna" && pojam != "trokrevetna" && pojam != "apartman" && pojam != "zauzeta" && pojam != "slobodna");
+	poruka(1);
+	cout<<setw(5)<<"Broj sobe"<<setw(10)<<"Cijena"<<setw(15)<<"Velicina"<<setw(15)<<"Stanje"<<endl;
+	Soba *s = new Soba;
+	ifstream ulaz("sobe.txt");
+	if(ulaz.is_open()){
+		while(ulaz >> s->brojSobe >> s->cijena >> velicina_broj >> s->slobodna){
+			if(velicina_broj == 1) krevet = "jednokrevetna";
+			else if(velicina_broj == 2) krevet = "dvokrevetna";
+			else if(velicina_broj == 3) krevet = "trokrevetna";
+			else krevet = "apartman";
+			if(s->slobodna) stanje = "zauzeta";
+			else stanje = "slobodna";
+			if(pojam == krevet || pojam == stanje){
+				brojac++;
+				cout<<setw(5)<<s->brojSobe<<setw(12)<<s->cijena<<setw(18)<<krevet<<setw(15)<<stanje<<endl;
+			}
+		}
+		ulaz.close();
+		delete s;
+	}
+	else{
+		poruka(5);
+		return;
+	}
+	if(brojac == 0){
+		poruka(4);
+		return;
+	}
+	poruka(1);
+}
+void uredi_cjenovnik(){
+	int broj;
+	float nova_cijena;
+	int velicina_broj;
+	int br_soba = prebroj_clanove("sobe.txt");
+	do{
+		cout<<"Unesite broj sobe kojoj zelite promijeniti cijenu: ";
+		cin>>broj;
+		if(broj < 1 || broj > br_soba){
+			poruka(3);
+			cout<<"Samo u intervalu 1 - "<<br_soba<<endl;
+		}
+	}
+	while(broj < 1 || broj > br_soba);
+	do{
+		cout<<"Unesite novu cijenu sobe: ";
+		cin>>nova_cijena;
+		if(nova_cijena < 50){
+			poruka(3);
+			cout<<"Samo cijene vece od 50 KM!"<<endl;
+		}
+	}
+	while(nova_cijena < 50);
+	Soba *s = new Soba;
+	Soba *niz = new Soba[br_soba];
+	int br = 0;
+	int pozicija;
+	ifstream ulaz("sobe.txt");
+	if(ulaz.is_open()){
+		while(ulaz >> s->brojSobe >> s->cijena >> velicina_broj >> s->slobodna){
+			if(velicina_broj == 1) s->velicina = jednokrevetna;
+			else if(velicina_broj == 2) s->velicina = dvokrevetna;
+			else if(velicina_broj == 3) s->velicina = trokrevetna;
+			else s->velicina = predsjednickiApartman;
+			niz[br] = *s;
+			br++;
+			if(broj == s->brojSobe){
+				pozicija = br-1;
+			}
+		}
+		ulaz.close();
+		azuriraj_sobe(niz, br_soba, pozicija, nova_cijena);
+		delete s;
+		cout<<"Uspjesno ste promijenili cijenu sobe!"<<endl;
+	}
+	else{
+		poruka(5);
+		return;
+	}
+	
+}
+//funkcija koja mijenja cijenu u datoteci za unesenu sobu
+void azuriraj_sobe(Soba *niz, int br_soba, int pozicija, float nova_cijena){
+	ofstream izlaz("sobe.txt");
+	if(izlaz.is_open()){
+		for(int i = 0; i<br_soba; i++){
+			if(i == pozicija){
+				izlaz << niz[i].brojSobe << " " << nova_cijena << " " << niz[i].velicina << " " << niz[i].slobodna << endl;	
+			}
+			else{
+				izlaz << niz[i].brojSobe << " " << niz[i].cijena << " " << niz[i].velicina << " " << niz[i].slobodna << endl;
+			}
+		}
+	izlaz.close();
+	}
+	else{
+		poruka(5);
+		return;
 	}
 }
